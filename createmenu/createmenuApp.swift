70c03d2b11033c758cf13805a482d40661a986ae
vuslattt <vuslatcoskun2001@gmail.com>
2023-04-06 09:18:36 +0300

    Initial Commit

---
+++ createmenu/createmenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  createmenuApp.swift
+//  createmenu
+//
+//  Created by vuslat coşkun on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct createmenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ createmenuUITests/createmenuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  createmenuUITestsLaunchTests.swift
+//  createmenuUITests
+//
+//  Created by vuslat coşkun on 6.04.2023.
+//
+
+import XCTest
+
+final class createmenuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
