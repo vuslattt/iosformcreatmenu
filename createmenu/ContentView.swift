639d2686987c346fa08bf1f0e248b870c44c6556
vuslattt <vuslatcoskun2001@gmail.com>
2023-04-09 22:02:04 +0300

    formview

--- createmenu/ContentView.swift
+++ createmenu/ContentView.swift
@@ -7,20 +7,106 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+final class IdentificableGenericForm: Identifiable , ObservableObject{
+    @Published var id = UUID()
+    @Published  var name : String
+    @Published  var price: String
+    @Published  var currencyunit: String
+    @Published var description : String
+    @Published var genericForms : [IdentificableGenericForm]
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    init(id: UUID = UUID(), name: String, price: String, currencyunit: String, description: String) {
+        self.id = id
+        self.name = name
+        self.price = price
+        self.currencyunit = currencyunit
+        self.description = description
+        self.genericForms = [IdentificableGenericForm(name: "", price: "", currencyunit: "", description: "")]
     }
+    
 }
+struct ContentView: View {
+    
+    @State private var name: String = ""
+    @State private var price: String = ""
+    @State private var currencyunit: String = ""
+    @State private var description: String = ""
+    
+    init(name: String, price: String, currencyunit: String, description: String) {
+        self.name = name
+        self.price = price
+        self.currencyunit = currencyunit
+        self.description = description
+    }
+    
+    var body: some View {
+    
+            VStack {
+                Text("Menü Oluştur")
+                    .font(.title)
+                TextField(
+                    "Menü Adı",
+                    text: $name
+                )
+                .disableAutocorrection(true)
+                TextField(
+                    "Fiyatı",
+                    text: $price
+                )
+                .disableAutocorrection(true)
+                TextField(
+                    "Para Birimi",
+                    text: $currencyunit
+                )
+                .disableAutocorrection(true)
+                TextField(
+                    "Açıklama",
+                    text: $description
+                )
+                Spacer()
+                    .disableAutocorrection(true)
+            }
+            .textFieldStyle(.roundedBorder)
+            
+       
+            
+            
+            HStack(spacing: 20) {
+                Button(action: {
+                }) {
+                    Text("Kaydet")
+                }
+                .buttonStyle(MyActionButtonStyle())
+                
+                Button(action: {
+                }) {
+                    Text("Yeni Ekle")
+                }
+                .buttonStyle(MyActionButtonStyle())
+            }
+            
+            .padding(.horizontal, 20)
+        }
+    
+    
+}
+    
+    struct MyActionButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .font(.headline.bold())
+                .foregroundColor(.white)
+                .frame(height: 38)
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .cornerRadius(9)
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView(name: "", price: "", currencyunit: "", description: "")
+        }
+    }
+
--- createmenu/createmenuApp.swift
+++ createmenu/createmenuApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct createmenuApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(name: "", price: "", currencyunit: "", description: "")
         }
     }
 }
